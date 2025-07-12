67f4f432f8a2b8202e395b58c1d3dfc48daf1dcf
wasapippi <wasapippi@gmail.com>
2025-07-12 22:56:42 +0900

    basePath変更

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const nextConfig = {
   output: 'export', // GitHub Pages対応のための静的エクスポート
   trailingSlash: true, // 出力パス末尾にスラッシュをつける（GH Pages向け）
-    basePath: '/svwb_utilities', // GitHubリポジトリの名前に置き換える
+    basePath: '/svwb_utilities/out', // GitHubリポジトリの名前に置き換える
   images: { unoptimized: true }, // Mantineの画像周り最適化を避ける
 };
 
